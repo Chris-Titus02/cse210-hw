a83a5f187a40244d61684e5be4dcc762afc8be7b
christitus <christophertitus1105@gmail.com>
2026-09-24 10:15:44 -0600

    Number Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Generate a random number between 1 and 100
+        Random random = new Random();
+        int randNum = random.Next(1, 101);
+
+        //Console.WriteLine(randNum);
+
+        //While Guess != Num,
+        int guess = 0;
+        while (guess != randNum)
+        {
+            //Get Guess from user input: What is your guess?
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            //Tell user if its higher, lower, or correct
+            if (guess > randNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (guess < randNum)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed Corectly");
+            }
+        }
+
+
+ 
     }
 }
