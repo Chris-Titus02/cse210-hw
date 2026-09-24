42d0e438eadad8cd9e3e4a0832e5d3421e6dbf90
christitus <christophertitus1105@gmail.com>
2026-09-23 21:25:29 -0600

    User inputs Grade, Outputs Letter Grade, Tells the user if you passed or failed, Checks if Grade is Valid

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask User for their Grade Percentage
+        Console.WriteLine("What is your Grade Percentage? ");
+        int gradePer = int.Parse(Console.ReadLine());
+
+        //Check if the Grade Percentage is Valid
+        if (gradePer >= 0 && gradePer <=100)
+        {
+            //Print the appropriate Letter Grade using IF/ELIF/ELSE statements
+            string letterGrade = "";
+
+            if (gradePer <= 100 && gradePer >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (gradePer >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (gradePer >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (gradePer >= 60)
+            {
+                letterGrade = "D";
+            }
+            else if (gradePer >= 0)
+            {
+                letterGrade = "F";
+            }
+
+            Console.WriteLine($"Your Letter Grade is: {letterGrade}");
+
+            //If Grade Percentage >= 70 print "Passed the class" else "Try Harder Bud"
+
+            if(gradePer >= 70)
+            {
+                Console.WriteLine("You passed the class!");
+            }
+            else
+            {
+                Console.WriteLine("Try Harder Bud");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Letter Grade, Please Try Again");
+        }
+
     }
 }
